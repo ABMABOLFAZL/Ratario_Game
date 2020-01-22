389b02fd71c1489e91c1ce41f42b17f264c72e04
ABMABOLFAZL <48096301+ABMABOLFAZL@users.noreply.github.com>
2020-01-22 19:17:54 +0330

    0.0.3 Player movement and jump and simple crouch done just enemy movement left
    enemy movement left and obstacle improvments and Lava thing

---
+++ Ratario/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player Movement")]
+    // player movement speed
+    [SerializeField]
+    [Range(0.1f,30.0f)]private float _speed = 10f;
+    [SerializeField]
+    [Range(0.1f,10.0f)]private float _jumpPower = 2f;
+    private float horizontalInput;
+    private Rigidbody _playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRB = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Moves the player right and left by the horizontal input.
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime); 
+        
+        // if player presses space jumpes
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRB.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+        }
+
+        // if player presses c it coruches
+        if (Input.GetKeyDown(KeyCode.C) & Input.GetKey(KeyCode.C))
+        {
+            Vector3 crouchScale = new Vector3(transform.localScale.x,0.35f,transform.localScale.z);
+            transform.localScale = crouchScale;
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            Vector3 afterCrouchScale = new Vector3(transform.localScale.x,0.50f,transform.localScale.z);
+            transform.localScale = afterCrouchScale;
+        }
+    }
+}

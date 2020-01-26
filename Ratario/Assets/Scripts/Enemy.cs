f2885da3cbdac6bf76f188f14f2d5920391e5a02
ABMABOLFAZL <48096301+ABMABOLFAZL@users.noreply.github.com>
2020-01-26 19:46:29 +0330

    0.0.4 Simple Enemy Movement
    needs fix

---
+++ Ratario/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pos2Object;
+    private Vector3 _pos1;
+    private Transform _pos2;
+    [SerializeField]
+    private float _patrolWaitTime = 3f;
+    private bool _isOnPos1 = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pos1 = gameObject.transform.position;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyPatrol();
+    }
+
+    private void EnemyPatrol()
+    {
+        if (transform.position == _pos1)
+        {
+            /* StartCoroutine (PatrolWaitPos2BackPos1()); */
+        }
+    }
+    /* IEnumerator PatrolWaitPos2BackPos1()
+    {
+        if (_isOnPos1)
+        {
+            transform.position = _pos2;
+            yield return new WaitForSeconds(_patrolWaitTime);
+            if (transform.position == _pos2)
+            {
+                transform.position = _pos1;
+                _isOnPos1 = false;
+            }     
+        }
+    } */
+}
